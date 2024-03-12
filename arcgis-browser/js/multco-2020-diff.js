a0c4ada2f7a3fb11b7b80196e27d13980d238a96
Kevin Wang <xinju.wang@portlandoregon.gov>
2024-03-12 10:35:29 -0700

    Fix bug

--- arcgis-browser/js/multco-2020-diff.js
+++ arcgis-browser/js/multco-2020-diff.js
@@ -112,7 +112,7 @@ legend.onAdd = function (map) {
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            grades[i] + ((grades[i + 1] !== undefined) ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
     return div;
